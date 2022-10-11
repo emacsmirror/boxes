5650233d4e1fae125a648bd78e5e2d245eac5d91
Thomas Jensen <boxes@thomasjensen.com>
2022-10-11 22:05:20 +0200

    foo

--- src/bxstring.c
+++ src/bxstring.c
@@ -2,20 +2,15 @@
  * boxes - Command line filter to draw/remove ASCII boxes around text
  * Copyright (c) 1999-2021 Thomas Jensen and the boxes contributors
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License, version 2, as published
- * by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License, version 3, as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
-* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 
 /*
@@ -23,10 +18,44 @@
  */
 
 #include "config.h"
+
 #include "bxstring.h"
+#include "tools.h"
+#include "unicode.h"
 
 
-/* TODO */
+
+bxstr_t *from_ascii(char *pAscii)
+{
+    if (pAscii == NULL) {
+        bx_fprintf(stderr, "%s: internal error: from_ascii() called with NULL\n", PROJECT);
+        return NULL;
+    }
+    if (strchr(pAscii, '\t') != NULL) {
+        bx_fprintf(stderr, "%s: internal error: from_ascii() called with tabs: \"%s\"\n", PROJECT, pAscii);
+        return NULL;
+    }
+
+    bxstr_t *result = (bxstr_t *) calloc(1, sizeof(bxstr_t));
+    result->memory = u32_strconv_from_arg(pAscii, "ASCII");
+    if (result->memory == NULL) {
+        BFREE(result);
+        return NULL;
+    }
+    result->ascii = pAscii;
+
+    size_t num_esc = 0;
+    char *ascii_copy;
+    size_t *map;
+    result->num_chars_invisible = count_invisible_chars(result->memory, &num_esc, &ascii_copy, &(map));
+    BFREE(ascii_copy);
+
+    result->num_bytes = strlen(pAscii);
+    result->num_chars = result->num_bytes;
+
+    /* TODO convert map to uint32_t **visible_char */
+}
+
 
 
 /*EOF*/                                          /* vim: set cindent sw=4: */
--- src/bxstring.h
+++ src/bxstring.h
@@ -2,20 +2,15 @@
  * boxes - Command line filter to draw/remove ASCII boxes around text
  * Copyright (c) 1999-2021 Thomas Jensen and the boxes contributors
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License, version 2, as published
- * by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License, version 3, as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
-* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 
 /*
@@ -25,13 +20,32 @@
 #ifndef BXSTRING_H
 #define BXSTRING_H
 
+#include <unitypes.h>
+
 
 typedef struct {
-    /*TODO*/
-} bxstring_t;
+    uint32_t  *memory;        /* Immutable. Pointer to the original memory area for the string */
+    char      *ascii;         /* Immutable. ASCII version of the original string, tabs expanded, invisible characters removed, multi-byte chars replaced with one or more 'x'. CHECK remove this eventually */
+    size_t     indent;        /* Immutable. Number of spaces at the beginning of the original string */
+    size_t     num_bytes;     /* Immutable. Total number of bytes occupied in memory by the original string */
+    size_t     num_chars;     /* Immutable. Number of characters in the original string (visible + invisible) */
+    size_t     num_chars_invisible; /* Immutable. Number of invisible characters in the original string */
+    size_t     trailing;      /* Immutable. Number of trailing spaces in the original string */
+    uint32_t **first_char;    /* Immutable. Array of pointers to the first actual character (possibly invisible) of each visible character */
+    uint32_t **visible_char;  /* Immutable. Array of pointers to the visible characters themselves */
+    int        offset_start;  /* Number of visible characters to cut from the beginning of the string. Can be negative, in which case spaces will be prepended. */
+    int        offset_end;    /* Number of visible characters to cut from the end of the string. Can be negative, in which case spaces will be appended. */
+} bxstr_t;
+
+
+/**
+ * Construct a `bxstr_t` from an ASCII string.
+ * @param ascii the ASCII string, which does not contain tabs
+ * @return a pointer to a new `bxstr_t` for which memory has been allocated
+ */
+bxstr_t *from_ascii(char *ascii);
 
 /* TODO */
-
 
 #endif
 
